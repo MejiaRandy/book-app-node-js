14e19097d0068fd8277d7535c098dbcb2ccf67f2
MejiaRandy <randyamejia@hotmail.com>
2024-07-18 23:17:59 -0400

    Fix: Error when starting the app

--- routes/bookRoutes.js
+++ routes/bookRoutes.js
@@ -1,19 +1,26 @@
+// routes/bookRoutes.js
 const express = require('express');
 const router = express.Router();
 const bookController = require('../controllers/bookController');
 const multer = require('multer');
 const upload = multer({ dest: 'public/images/' });
 
+// List all books
 router.get('/', bookController.listBooks);
 
+// Form to create a new book
 router.get('/create', bookController.createBookForm);
 
+// Create a new book
 router.post('/create', upload.single('coverImage'), bookController.createBook);
 
+// Form to edit a book
 router.get('/edit/:id', bookController.editBookForm);
 
+// Edit a book
 router.post('/edit/:id', upload.single('coverImage'), bookController.editBook);
 
+// Delete a book
 router.get('/delete/:id', bookController.deleteBook);
 
 module.exports = router;
